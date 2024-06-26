2da268c1ece9d4c33bf906b9a6d5949b374c3e4f
LAB_E503 <labe503@continental.edu.pe>
2024-06-26 07:55:46 -0500

    correccion

--- fibonacci.py
+++ fibonacci.py
@@ -5,6 +5,7 @@ def fibonacci(n):
 
     valores.remove(0)
     return(valores)
+
 def n_termino(n):
     serie = fibonacci(n)
     valor = serie[-1]
@@ -15,16 +16,30 @@ def suma_n_terminos(n):
     suma = sum(lista)
     return suma
 
-# caso 1 ejercicio A
-a = int(input("Ingrese la cantidad de terminos que desea: "))
-print("Ejercicio A, n términos de la serie: ",fibonacci(a))
 
-#Caso 1 ejercicio B
-b = int(input("Ingrese n termino que desea: "))
-print("Ejercicio B, n término de la serie: ",n_termino(b))
-
-#Caso 1 ejercicio C
-c = int(input("Ingrese la cantidad de terminos que desea sumar: "))
-print("Ejercicio A, suma de n términos de la serie: ",suma_n_terminos(c))
+while True:
+    print("*"*20)
+    print("Ejercicios:")
+    print("""          
+1-> Ejercicio A
+2-> Ejercicio B
+3-> Ejercicio C
+4-> Salir
+""")
+    option = int(input("-> "))
+    if option == 1:
+        # caso 1 ejercicio A
+        a = int(input("Ingrese la cantidad de terminos que desea: "))
+        print("Ejercicio A, n términos de la serie: ",fibonacci(a))
+    elif option == 2:
+        #Caso 1 ejercicio B
+        b = int(input("Ingrese n termino que desea: "))
+        print("Ejercicio B, n término de la serie: ",n_termino(b))
+    elif option == 3:
+        #Caso 1 ejercicio C
+        c = int(input("Ingrese la cantidad de terminos que desea sumar: "))
+        print("Ejercicio A, suma de n términos de la serie: ",suma_n_terminos(c))
+    else:
+        break
 
     
